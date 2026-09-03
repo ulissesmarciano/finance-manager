622a382eb256ab8c86244dd65203aa1e63b39dc5
ulissesmarciano <ulissesmarcianodev@gmail.com>
2026-09-03 19:26:58 -0300

    feat: Implement router config

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./screens/home/home').then(m => m.Home),
+  },
+  {
+    path: 'transactions',
+    loadComponent: () => import('./screens/transactions/transactions').then(m => m.Transactions),
+  },
+  {
+    path: 'cards-accounts',
+    loadComponent: () => import('./screens/cards-accounts/cards-accounts').then(m => m.CardsAccounts),
+  },
+  {
+    path: 'targets-quotes',
+    loadComponent: () => import('./screens/targets-quotes/targets-quotes').then(m => m.TargetsQuotes),
+  },
+  {
+    path: 'configuration',
+    loadComponent: () => import('./screens/configuration/configuration').then(m => m.Configuration),
+  }
+];
