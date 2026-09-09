6cc4d834e5b51023edf103f8afeef10f83b6539c
ulissesmarciano <ulissesmarcianodev@gmail.com>
2026-09-09 17:40:32 -0300

    feat: create metric card component type

--- src/app/components/shared/card-component/metric/metric.ts
+++ src/app/components/shared/card-component/metric/metric.ts
@@ -1,5 +1,13 @@
 import { Component, Input } from '@angular/core';
 
+type CardType = 'summary' | 'metrics';
+interface MetricClasses {
+  container: string;
+  label: string;
+  value: string;
+  foot?: string;
+}
+
 @Component({
   imports: [],
   selector: 'app-metric',
@@ -8,4 +16,24 @@ import { Component, Input } from '@angular/core';
 export class Metric {
   @Input() label = '';
   @Input() value = '';
+  @Input() foot = '';
+  @Input() variant: CardType = 'summary';
+
+  variantClasses(): MetricClasses {
+    const variants: Record<CardType, MetricClasses> = {
+      summary: {
+        container: 'flex flex-col',
+        label: 'text-primary-foreground opacity-80 text-14',
+        value: 'text-primary-foreground text-18 font-bold',
+      },
+      metrics: {
+        container: 'flex flex-col',
+        label: 'text-muted-foreground text-14',
+        value: 'text-foreground text-24 font-bold',
+        foot: 'text-muted-foreground text-12',
+      },
+    };
+
+    return variants[this.variant];
+  }
 }
