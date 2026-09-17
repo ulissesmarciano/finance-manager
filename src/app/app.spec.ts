4019957406e7cdaa107acb7dba833768d549f146
ulissesmarciano <ulissesmarcianodev@gmail.com>
2026-09-17 20:50:22 -0300

    test: creap app test

---
+++ src/app/app.spec.ts
@@ -0,0 +1,24 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { describe, beforeEach, it, expect } from 'vitest';
+import { App } from './app';
+import { provideRouter } from '@angular/router';
+
+describe('App', () => {
+  let component: App;
+  let fixture: ComponentFixture<App>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [App],
+      providers: [provideRouter([])],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(App);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
