891e121790c9e8b7b418cdcf41b576f6fb5b0799
ulissesmarciano <ulissesmarcianodev@gmail.com>
2026-09-24 23:19:43 -0300

    feat: create not found page component

--- src/app/components/shared/not-found/not-found.ts
+++ src/app/components/shared/not-found/not-found.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
+import { Link } from '../link/link';
 
 @Component({
-  imports: [],
+  imports: [Link],
   selector: 'app-not-found',
   templateUrl: './not-found.html',
 })
